d8069813beb9ca860792f54e8c62fef2a87af50b
anish-sharan <anishsharan2000@gmail.com>
2022-12-16 23:52:44 +0530

    added initial route

--- client/navigation/UserStack.js
+++ client/navigation/UserStack.js
@@ -8,9 +8,11 @@ function UserStack() {
     const Stack = createNativeStackNavigator();
     return (
         <Stack.Navigator
+            initialRouteName="SignupScreen"
             screenOptions={{
                 headerShown: false
-            }}>
+            }}
+            >
             <Stack.Screen name="BottomTabStack" component={BottomTabStack} />
             <Stack.Screen name="LoginScreen" component={LoginScreen} />
             <Stack.Screen name="SignupScreen" component={SignupScreen} />
