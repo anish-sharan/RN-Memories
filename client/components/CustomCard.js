2edd159a31c5c993324d508df9e73c949021fbdd
anish-sharan <anishsharan2000@gmail.com>
2023-02-04 23:27:08 +0530

    changed custom card

--- client/components/CustomCard.js
+++ client/components/CustomCard.js
@@ -1,60 +1,27 @@
-import React from 'react'
-import { View, StyleSheet, Text, Image } from 'react-native';
-import FontSize from '../assets/FontSize.js';
-import Colors from '../assets/Colors.js';
-import { Button } from 'react-native-paper'
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Avatar, Card, Text } from "react-native-paper";
 
+const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 const CustomCard = ({ style, title, description }) => {
-    return (
-        <View style={[styles.container, style]}>
-            <View style={styles.cardBackground}>
-                <Text style={styles.heading}>{title}</Text>
-                <Text style={styles.description}>{description}</Text>
-                <Button icon='camera' style={styles.icon} />
-                <Image source={require('../assets/favicon.png')} style={styles.image} />
-            </View>
-        </View>
-    )
-}
+  return (
+    <View style={[styles.container, style]}>
+      <Card>
+        <Card.Cover source={{ uri: "https://picsum.photos/700" }} />
+        <Card.Content>
+          <Text variant="titleLarge">{title}</Text>
+          <Text variant="bodyMedium">{description}</Text>
+        </Card.Content>
+      </Card>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        fontSize: FontSize.Heading,
-        alignSelf: 'center',
-        width: '90%',
-        height: '40%'
-    },
-    cardBackground: {
-        backgroundColor: Colors.light,
-        width: '100%',
-        height: '60%',
-        borderRadius: 10,
-        flex: 3,
-        // flexDirection: 'row'
-    },
-    image: {
-        backgroundColor: Colors.dark,
-        borderRadius: 10,
-        height: '100%',
-        width: '30%',
-        zIndex: 1,
-        position: 'absolute'
-    },
-    heading: {
-        marginLeft: '32%',
-        fontSize: 22
-    },
-    description: {
-        marginLeft: '32%',
-        fontSize: 20
-    },
-    date: {
-        marginLeft: '32%',
-        fontSize: 15
-    },
-    icon: {
-        marginLeft: '85%',
-    }
-})
+  container: {
+    alignSelf: "center",
+    width: "90%",
+  },
+});
 
 export default CustomCard;
